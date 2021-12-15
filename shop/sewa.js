95eefb82ca4848da305a2d2a97476d9a06e91072
Elaina1118 <95533021+Elaina1118@users.noreply.github.com>
2021-12-15 07:07:53 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -8,21 +8,7 @@ return `「 *PRICE LIST* 」
 ⬡ 3 MINGGU = ${___minggu}
 ⬡ 1 BULAN = ${_bulan}
 ⬡ PERMANEN = ${_permanen}
-⬡ PERMANEN + USER PREM = ${__permanen}
-
-
-⬣ PRICE JADIBOT
-
-⬡ JADI BOT PERMANEN 25K
-
-
-⬣ MINAT? PM
-⬡ wa.me/62887435047326
-
-
-NOTE : 
-Bot On 24 Jam Karna Di Run Menggunakan Heroku
-Reset Data Setiap Jam 02:00 `
+⬡ PERMANEN + USER PREM = ${__permanen}`
 	}
 
 exports.pc_sewa = pc_sewa
